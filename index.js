a19390cc55054010287a6d3179870d0bd5c360e4
Damilola Kolawole <damilolamiraclek@gmail.com>
2023-06-02 08:41:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,19 +15,28 @@ function calculation() {
         console.log(tipPercentage.value);
     })
 
-    let percent = tipPercentage.value
-    let amount = billAmount.value
-    let answer = (percent / 100) * amount
-    let secondAnswer = amount
-    let solution = answer + "+" +  secondAnswer
-    return eval(solution);
+    if (billAmount.value <= 100) {
+        tipPercentage.style.display = "none"
+        let lessValue = billAmount.value + "+"+ "10"
+        let result = eval(lessValue)
+        return ( result + " You can only get N10 as a tip, please buy items worth more than N100")
+    } else {
+        let percent = tipPercentage.value
+        let amount = billAmount.value
+        let answer = (percent / 100) * amount
+        let secondAnswer = amount
+        let solution = answer + "+" +  secondAnswer
+        return eval(solution);
+    }
+
+    
     
     
 }
 
 
 button.addEventListener("click", ()=>{
-    total.textContent = "Total: " + "$" + calculation();
+    total.textContent = "Total: " + "N" + calculation();
 })
 
 
